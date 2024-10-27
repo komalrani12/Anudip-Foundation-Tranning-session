2e3bd0a495f5678f588d80aadce8fe6ccfef6c4e
komalrani12 <167525085+komalrani12@users.noreply.github.com>
2024-10-27 23:31:13 +0530

    assignment2

---
+++ A23.java
@@ -0,0 +1,12 @@
+public class A23 {
+    public static void main(String[] args) {
+        boolean c=4>7;
+        boolean d=10>5;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(c&&d);
+        System.out.println(c||d);
+        System.out.println(c!=d);
+    }
+    
+}
---
+++ A24.java
@@ -0,0 +1,11 @@
+public class A24 {
+    public static void main(String[] args) {
+        int num=5;
+        int ans=0;
+        for(int i=0; i<3; i++){
+            ans+=num;
+        }
+        System.out.println(ans);
+    }
+    
+}
