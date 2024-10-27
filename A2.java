b3c718e0f355589a564d48c93a9c1e44ee4b51cc
komalrani12 <167525085+komalrani12@users.noreply.github.com>
2024-10-27 23:27:43 +0530

    Assignment 2

---
+++ A2.java
@@ -0,0 +1,17 @@
+public class A2 {
+    public static void main(String[] args) {
+        int a=10; 
+        int b=20;
+        int sum=a+b;
+        System.out.println(sum);
+        int diff=a-b;
+        System.out.println(diff);
+        int  mul=a*b;
+        System.out.println(mul);
+        int rem=a%b;
+        System.out.println(rem);
+        int qutient=a/b;
+        System.out.println(qutient);
+    }
+    
+}
